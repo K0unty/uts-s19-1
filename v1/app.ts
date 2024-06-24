9db0cc2b89ba26f33d84cc26b078442860a3a5a0
K0unty <119057948+K0unty@users.noreply.github.com>
2024-06-24 11:34:38 +0000

    .

--- v1/app.ts
+++ v1/app.ts
@@ -11,10 +11,3 @@ age = 5;
 console.log(age);
 
 // Write async await function
-
-const getCat = async () => {
-  const response = await fetch("https://api.thecatapi.com/v1/images/search");
-  const data = await response.json();
-  console.log(data);
-};
-getCat();
